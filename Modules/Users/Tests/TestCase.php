25208ff368ee4a4cb79552a89d7ad0e5cfeaa5c2
Farid Teymouri(icqx) <farid.teymouri@outlook.com>
2026-02-15 20:25:45 +0330

    fix(users): fix test infrastructure by extending root TestCase
    - Remove redundant CreatesApplication trait from module tests
    - Extend Laravel's root TestCase to inherit proper test bootstrapping
    - Resolves 'Trait not found' fatal error during test execution

--- Modules/Users/Tests/TestCase.php
+++ Modules/Users/Tests/TestCase.php
@@ -2,18 +2,6 @@
 
 namespace Modules\Users\Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tests\TestCase as BaseTestCase;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;   ← REMOVE or COMMENT OUT
-
-abstract class TestCase extends BaseTestCase
-{
-    use CreatesApplication;
-    // use RefreshDatabase;   ← REMOVE or COMMENT OUT
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-        // Do NOT add any Artisan::call('migrate') calls here
-    }
-}
+abstract class TestCase extends BaseTestCase {}
